3320ea4de667f55c1f242260aaa9fc3c1bf00f2a
Darrius <Darrius.x.leaphart@gmail.com>
2018-11-02 02:39:41 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,14 +7,22 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
 
-test "email addresses should be unique" do
+  test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
 
-test "authenticated? should return false for a user with nil digest" do
+  test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
-end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+end 
